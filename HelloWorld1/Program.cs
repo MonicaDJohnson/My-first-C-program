ebd6577cbb473046c8a687571c430dfc20293da1
MonicaDJohnson <johnsondmonica290@gmail.com>
2018-05-23 09:50:46 -0400

    added for loops

--- HelloWorld1/Program.cs
+++ HelloWorld1/Program.cs
@@ -155,14 +155,14 @@ namespace HelloWorld1
             //            }//end switch
 
             //// Part Twelve Array examples
-            //    string[] foodList = new string[5];
-            //    foodList[0] = "Milk";
-            //    foodList[1] = "Fruit";
-            //    foodList[2] = "Meat";
-            //    foodList[3] = "Wine";
-            //    foodList[4] = "Bread";
+            string[] foodList = new string[5];
+            foodList[0] = "Milk";
+            foodList[1] = "Fruit";
+            foodList[2] = "Meat";
+            foodList[3] = "Wine";
+            foodList[4] = "Bread";
             //    Console.WriteLine("{0}, {1}, {2}, {3}, {4}", foodList[0], foodList[1], foodList[2], foodList[3], foodList[4]);
-            //int[] foodAmount = new int[] { 1000, 1000, 2000, 10000, 1500 };
+            int[] foodAmount = new int[] { 1000, 1000, 2000, 10000, 1500 };
             //Console.WriteLine(foodAmount[0] + ", " + foodAmount[1] + ", " + foodAmount[2] + ", " + foodAmount[3] + ", " + foodAmount[4] + ", ");
             //Console.WriteLine(foodList.Length);
 
@@ -214,31 +214,18 @@ namespace HelloWorld1
             //    Console.WriteLine("Add another? Type exit to quit.");
             //    action = Console.ReadLine();
                   //}
-            do
+
+            ////Part fourteen for-loop examples
+            //for (int i = 0; i < foodAmount.Length; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            
+            for (int i = 0; i < foodAmount.Length; i++)
             {
-                Console.WriteLine("What is your rank soldier?");
-                string rank = Console.ReadLine();
-
-                Console.WriteLine("What is your age soldier?");
-                int age = int.Parse(Console.ReadLine());
-                Console.WriteLine("What is your job soldier?");
-                string job = Console.ReadLine();
-
-                if ((rank == "officer") || (age <= 26))
-                {
-                    Console.WriteLine("Get your gear, kiss your family goodbye, put on your marching boots.");
-                }
-                else if ((job == "cook") || (age >= 26))
-                {
-                    Console.WriteLine("My army has to eat. Pack your pots and pans");
-                }
-                else
-                {
-                    Console.WriteLine("Sorry you are staying home.");
-                }
-                Console.WriteLine("Add another? Type exit to quit");
-                action = Console.ReadLine();
-            } while (action != "exit");
+                Console.WriteLine("Enter a value for " + foodList[i]);
+                foodAmount[i] = int.Parse(Console.ReadLine());
+            }
 
 
 
